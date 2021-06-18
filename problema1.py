bab324b2b6a81c122886962470a2c951a5ae2598
leviathan2709 <86124503+leviathan2709@users.noreply.github.com>
2021-06-18 13:18:54 -0500

    Add files via upload
    se añadió la solución al problema 1 en el reame

---
+++ problema1.py
@@ -0,0 +1,7 @@
+def areaTriangulo (base,altura):
+
+    areaTriangulo= (base*altura)/2
+    return areaTriangulo
+
+a1= (areaTriangulo(50,30))
+print ("el area del triangilo es: " + (str(a1)) + " cm")
